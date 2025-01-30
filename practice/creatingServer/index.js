0568095959df8b07b30acd75117933d7b906e1ad
Aryan Gupta <guptaaryan2050@gmail.com>
2025-01-30 21:26:33 +0530

    learn how to create web server

--- practice/creatingServer/index.js
+++ practice/creatingServer/index.js
@@ -1,14 +1,24 @@
 const http = require("http");
 const fs = require("fs");
 
-const myServer = http.createServer((req,res) => {
-    console.log("new req received");
-    const log = `${Date.now()} : new request received\n`;
-    fs.appendFile('./log.txt',log,(err,data) => {
-        res.end("again hello from server");
-    })
-    
+const myServer = http.createServer((req, res) => {
+  // console.log("new req received");
+  const log = `${Date.now()} :${req.url} new request received\n`;
+  fs.appendFile("./log.txt", log, (err, data) => {
+    switch (req.url) {
+      case "/":
+        res.end("This is Homepage");
+        break;
+      case "/about":
+        res.end("this is about page");
+        break;
 
-})
+      default:
+        res.end("404 not found");
+    }
+  });
+});
 
-myServer.listen(8000,() => {console.log("server started")});
+myServer.listen(8000, () => {
+  console.log("server started");
+});
